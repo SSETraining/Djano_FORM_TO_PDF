00379cbc1e8925e0837782ea4bc17248d0072ced
Salman Sajid <salmansajidsattar@gmail.com>
2023-08-25 05:00:43 +0500

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
+# make migrations
+pip install -r requirements.txt
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
 echo " BUILD END"
